c6097b10c394d31442a188e7d6c16faa1789bc9e
tarasovc47 <tarasovc47@live.ru>
2022-01-08 19:45:15 +0500

    При переходе по сслыке (которая формируется из slug) мы попадаем в /authors/[slug]

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Author;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::query()->simplePaginate(10);
+        return view('author.index', compact('authors'));
+    }
+    public function one($slug)
+    {
+        /*$author = Author::query()->where('slug', '=', $slug);
+        return view('author.view', compact('author'));*/
+        return view('author.view', [
+            'author' => Author::query()->where('slug', '=', $slug)->first(),
+        ]);
+    }
 }
